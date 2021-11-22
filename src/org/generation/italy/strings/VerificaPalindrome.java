077e38e708a963b1a0a112a06b1135f02f2cf206
Shaw <michele_aliffi@yahoo.it>
2021-11-22 14:54:51 +0100

    Fine progetto.

--- src/org/generation/italy/strings/VerificaPalindrome.java
+++ src/org/generation/italy/strings/VerificaPalindrome.java
@@ -1,10 +1,31 @@
 package org.generation.italy.strings;
 
+import java.util.Scanner;
+
 public class VerificaPalindrome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		String word = "";
+		
+		System.out.print("Inserisci di seguito una parola: ");
+		word = scan.nextLine();
+		String wordReverse = "";
+		
+		for(int i = word.length() - 1; i >= 0; i--) {
+			wordReverse = wordReverse + word.charAt(i);
+		}
+		
+		if(wordReverse.equals(word)) {
+			System.out.println("La parola inserita è palindroma");
+		} else {
+			System.out.println("Non è una parola palindroma");
+		}
+		
+		System.out.println("La parola è lunga: " + word.length());
+		
+		scan.close();
 	}
 
 }
